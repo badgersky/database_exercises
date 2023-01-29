38c4c93907ff3129a151966005a0e37e18fc29d7
badgersky <szymonek.ptasznik@gmail.com>
2023-01-29 16:20:58 +0100

    code refactor, not only select instruction works

--- db_python/sql_utils.py
+++ db_python/sql_utils.py
@@ -9,19 +9,26 @@ PASSWORD = 'coderslab'
 def execute_sql(db, sql_task):
     try:
         cnx = connect(user=USER, host=HOST, password=PASSWORD, database=db)
-        with cnx.cursor() as cursor:
-            cursor.execute(sql_task)
+        cnx.autocommit = True
+        cursor = cnx.cursor()
+        cursor.execute(sql_task)
+        if sql_task[:6].lower() == 'select':
+            result = cursor.fetchall()
+        else:
             result = None
-            if cursor.rowcount > 0:
-                result = cursor.fetchall()
-
-    except OperationalError:
+    except:
         print('There is error in execute_sql')
     else:
         cursor.close()
         cnx.close()
         return result
 
+
+results = execute_sql('exercises_db', 'SELECT * FROM products;')
+for res in results:
+    print(res)
+
+execute_sql('exercises_db', "INSERT INTO products VALUES(4, 'i3', '5th generetion', 500, 5);")
 
 results = execute_sql('exercises_db', 'SELECT * FROM products;')
 for res in results:
